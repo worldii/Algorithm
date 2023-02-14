47180d14f232fe90f3505a2dd16eaada85c9746b
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-02-14 16:35:06 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 560 ms, Memory: 31800 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.java
@@ -14,93 +14,107 @@ public class Main {
 	public static int[] dx = { 0, 1, 0, -1 };
 	public static int[] dy = { 1, 0, -1, 0 };
 	public static ArrayDeque<Integer> queue = null;
-    static StringBuilder sb = new StringBuilder();
+	static StringBuilder sb = new StringBuilder();
 
-	public static void rotate(int x, int y, int row, int col) {
-		// int row = 5 , col = 4
-		queue = new ArrayDeque<>();
+	public static void insertQueue(int x, int y, int row, int col) {
 		int curX = x;
 		int curY = y;
 		int cnt = 0;
+		int beforeX =x;
+		int beforeY =y;
+		int temp = arr[curX][curY];
+		
 		
 		for (int i = 0; i < col - 1; i++) {
-			queue.add(arr[curX][curY]);
-			curX = dx[cnt] + curX;
-			curY = dy[cnt] + curY;
+			curX = dx[cnt] + beforeX;
+			curY = dy[cnt] + beforeY;
+			arr[beforeX][beforeY] = arr[curX][curY];
+			beforeX = curX ;
+			beforeY  =curY;
 		}
 		cnt++;
 
 		for (int i = 0; i < row - 1; i++) {
-			queue.add(arr[curX][curY]);
 			curX = dx[cnt] + curX;
 			curY = dy[cnt] + curY;
+			arr[beforeX][beforeY] = arr[curX][curY];
+			beforeX = curX ;
+			beforeY  =curY;
 
 		}
 
 		cnt++;
 
 		for (int i = 0; i < col - 1; i++) {
-
-			queue.add(arr[curX][curY]);
 			curX = dx[cnt] + curX;
 			curY = dy[cnt] + curY;
+			arr[beforeX][beforeY] = arr[curX][curY];
+			beforeX = curX ;
+			beforeY  =curY;
 
 		}
 
 		cnt++;
 
 		for (int i = 0; i < row - 2; i++) {
-			queue.add(arr[curX][curY]);
 			curX = dx[cnt] + curX;
 			curY = dy[cnt] + curY;
+			arr[beforeX][beforeY] = arr[curX][curY];
+			beforeX = curX ;
+			beforeY  =curY;
+
 		}
-		queue.add(arr[curX][curY]);
+		arr[beforeX][beforeY] = temp;
 		cnt++;
-		
-		queue.add(queue.poll());
+	}
 
-		curX = x;
-		curY = y;
-		cnt = 0;
-		for (int i = 0; i < col - 1; i++) {
-			arr[curX][curY] = queue.poll();
-			curX = dx[cnt] + curX;
-			curY = dy[cnt] + curY;
-		}
-		cnt++;
-		for (int i = 0; i < row - 1; i++) {
+//	public static void insertArr(int x, int y, int row, int col) {
+//		int curX = x;
+//		int curY = y;
+//		int cnt = 0;
+//		for (int i = 0; i < col - 1; i++) {
+//			arr[curX][curY] = queue.poll();
+//			curX = dx[cnt] + curX;
+//			curY = dy[cnt] + curY;
+//		}
+//		cnt++;
+//		for (int i = 0; i < row - 1; i++) {
+//
+//			arr[curX][curY] = queue.poll();
+//			curX = dx[cnt] + curX;
+//			curY = dy[cnt] + curY;
+//
+//		}
+//		cnt++;
+//
+//		for (int i = 0; i < col - 1; i++) {
+//
+//			arr[curX][curY] = queue.poll();
+//			curX = dx[cnt] + curX;
+//			curY = dy[cnt] + curY;
+//		}
+//		cnt++;
+//
+//		for (int i = 0; i < row - 2; i++) {
+//			arr[curX][curY] = queue.poll();
+//
+//			curX = dx[cnt] + curX;
+//			curY = dy[cnt] + curY;
+//		}
+//		arr[curX][curY] = queue.poll();
+//	}
 
-			arr[curX][curY] = queue.poll();
-			curX = dx[cnt] + curX;
-			curY = dy[cnt] + curY;
+	public static void rotate(int x, int y, int row, int col) {
+		// int row = 5 , col = 4
+		insertQueue(x, y, row, col);
+	//	insertArr(x, y, row, col);
 
-		}
-		cnt++;
-
-		for (int i = 0; i < col - 1; i++) {
-
-			arr[curX][curY] = queue.poll();
-			curX = dx[cnt] + curX;
-			curY = dy[cnt] + curY;
-		}
-		cnt++;
-
-		for (int i = 0; i < row - 2; i++) {
-			arr[curX][curY] = queue.poll();
-
-			curX = dx[cnt] + curX;
-			curY = dy[cnt] + curY;
-		}
-		arr[curX][curY] = queue.poll();
-
-		
 	}
 
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
-
-		 n = Integer.parseInt(st.nextToken());
-		 m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		int r = Integer.parseInt(st.nextToken());
 		arr = new int[n][m];
 		checked = new int[n][m];
@@ -115,24 +129,24 @@ public class Main {
 		int cury = 0;
 		int curRow = n;
 		int curCol = m;
-		while (curRow>0 && curCol>0) {
-			int t = r%((curRow-1)*2 +(curCol-1)*2); 
-			for (int i = 0; i< t; i++) {
-			rotate(curx, cury, curRow, curCol);
+		while (curRow > 0 && curCol > 0) {
+			int t = r % ((curRow - 1) * 2 + (curCol - 1) * 2);
+			for (int i = 0; i < t; i++) {
+				rotate(curx, cury, curRow, curCol);
 			}
-			curx =curx+1;
-			cury = cury+1;
-			curRow-=2;
-			curCol -=2;
+			curx = curx + 1;
+			cury = cury + 1;
+			curRow -= 2;
+			curCol -= 2;
 		}
 
-		for(int i=0;i<n;i++) {
-        	for(int j=0;j<m;j++) {
-        		sb.append(arr[i][j]).append(" ");
-        	}
-        	sb.append("\n");
-        }
-        System.out.print(sb);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				sb.append(arr[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
 	}
 
 }
