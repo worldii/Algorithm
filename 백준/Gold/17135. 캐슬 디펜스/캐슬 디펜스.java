3a15001c263bf8916a40b7e9a3aaf7263627fada
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-02-23 17:04:09 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 136 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -31,121 +31,75 @@ public class Main {
     public static int getDis(int x, int y, int x2, int y2) {
         return Math.abs(x - x2) + Math.abs(y - y2);
     }
-
-    public static int[][] copy(int[][] arr) {
-        int[][] newArr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                newArr[i][j] = arr[i][j];
+    public static void killEne(int [][] arr ) {
+        for (int i = 0 ; i< n ; i++) {
+            for (int j = 0 ; j< m ; j++) {
+                if (arr[i][j] == 2) {
+                    arr[i][j]  =0;
+                }
             }
         }
-        return newArr;
     }
+    public static Cor selectEne(int x, int y, int[][]arr) {
+        Cor temp = null;
+        for (int i = 0 ; i<x ; i++) {
+            for (int j = 0  ; j<  m ; j++) {
+                if (arr[i][j] ==1 ) {
+                    int dis = getDis(i,j,x, y) ;
+                    if (dis <= d) {
 
-    public static void downArr(int[][] arr) {
-        int[][] newArr = copy(arr);
-        // 다운 !!
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = newArr[i - 1][j];
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            arr[0][i] = 0;
-        }
-
-    }
-
-    public static int shoot(int[] loc) {
-        Comparator<Cor> c = new Comparator<Cor>() {
-            @Override
-            public int compare(Cor o1, Cor o2) {
-                if (o1.dis != o2.dis) return Integer.compare(o1.dis, o2.dis);
-                else return Integer.compare(o1.y, o2.y);
-            }
-        };
-
-        PriorityQueue<Cor> q3 = new PriorityQueue<>(c);
-        PriorityQueue<Cor> q1 = new PriorityQueue<>(c);
-        PriorityQueue<Cor> q2 = new PriorityQueue<>(c);
-
-        int[][] newArr = copy(arr);
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int personX = n;
-            // 궁수 1,2,3;
-            int person1Y = loc[0];
-            int person2Y = loc[1];
-            int person3Y = loc[2];
-            boolean[][] checked = new boolean[n][m];
-            // 탐색하여 q에 가능한 것 다 넣음
-            q1.clear();
-            q2.clear();
-            q3.clear();
-
-            // 궁수 1
-            for (int t = 0; t < n; t++) {
-                for (int tt = 0; tt < m; tt++) {
-                    if (newArr[t][tt] == 1) {
-                        int dis = getDis(personX, person1Y, t, tt);
-                        if (dis <= d) {
-                            q1.add(new Cor(t, tt, dis));
-                        }
-                        dis = getDis(personX, person2Y, t, tt);
-                        if (dis <= d) {
-                            q2.add(new Cor(t, tt, dis));
-                        }
-                        dis = getDis(personX, person3Y, t, tt);
-                        if (dis <= d) {
-                            q3.add(new Cor(t, tt, dis));
-                        }
+                        if (temp == null) temp = new Cor(i,j, dis);
+                        if (temp.dis > dis) temp = new Cor(i,j,dis);
+                        if (temp.dis == dis && temp.y > j) temp = new Cor(i,j, dis );
                     }
                 }
             }
-
-            if (!q1.isEmpty()) {
-                Cor temp = q1.peek();
-                if (!checked[temp.x][temp.y]) {
-                    checked[temp.x][temp.y] = true;
-                    sum++;
-                    newArr[temp.x][temp.y] = 0;
-                }
-            }
-
-            if (!q2.isEmpty()) {
-                Cor temp = q2.peek();
-                if (!checked[temp.x][temp.y]) {
-                    checked[temp.x][temp.y] = true;
-                    sum++;
-                    newArr[temp.x][temp.y] = 0;
-                }
-            }
-
-            if (!q3.isEmpty()) {
-                Cor temp = q3.peek();
-                if (!checked[temp.x][temp.y]) {
-                    checked[temp.x][temp.y] = true;
-                    sum++;
-                    newArr[temp.x][temp.y] = 0;
-                }
-            }
-            // array 를 다운시켜줌
-             downArr(newArr);
-//                System.out.println("ARRAY DOWN");
-//             System.out.println(sum);
         }
+        return temp;
+    }
+    public static int shoot(int[] loc) {
+      //  System.out.println("LOC");
+        int [][] newArr = new int [n][m];
+        for (int i = 0 ; i< n ; i++) {
+            for (int j = 0 ; j< m ; j++) {
+                newArr[i][j] = arr[i][j] ;
+            }
+        }
+
+        int sum = 0;
+        boolean [][] checked = new boolean [n] [m] ;
+        int row = n;
+        for (int i = 0; i < n; i++) {
+            // 궁수 1,2,3;
+            List<Cor> enemy = new ArrayList<>();
+
+            for (int t = 0 ;t<3 ; t++) {
+                Cor selectEnemy =selectEne(row, loc[t],newArr);
+                if (selectEnemy == null ) continue;
+                enemy.add(selectEnemy);
+            }
+
+            for (int t = 0 ; t< enemy.size() ; t++) {
+                Cor selectEnemy = enemy.get(t);
+                //System.out.println("ENE"+selectEnemy);
+                if (!checked[selectEnemy.x][selectEnemy.y]) {
+                    checked[selectEnemy.x][selectEnemy.y] = true;
+                    sum++;
+                    newArr[selectEnemy.x][selectEnemy.y] = 0;
+                }
+            }
+          //  System.out.println(sum+" ");
+            row--;
+
+        }
+
 
         return sum;
     }
 
     public static void makeCombination(int cnt, int end, int start, int[] arr, boolean[] checked) {
         if (cnt == end) {
-//            for (int i = 0; i < cnt; i++) {
-//                System.out.print(arr[i] + " ");
-//            }
             int num = shoot(arr);
-        //    System.out.println(num);
             maxNum = Math.max(num, maxNum);
             return;
         }
