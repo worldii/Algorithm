8f288254715a0644df8c1eaa0a8a535d7e121a79
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-03-30 20:03:14 +0900

    [Gold II] Title: 문제집, Time: 448 ms, Memory: 47660 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -6,59 +6,60 @@ import java.util.*;
 
 public class Main {
 
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static StringTokenizer st = null;
-	public static int n;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st = null;
+    public static int n;
 
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		st= new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m  = Integer.parseInt(st.nextToken());
-		int []in =new int [n+1];
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] in = new int[n + 1];
 
-		ArrayList<ArrayList<Integer>> graph =new ArrayList<>();
-		for (int i = 0 ; i<= n ; i++) {
-			graph.add(new ArrayList<>());
-		}
-		for (int i = 0 ; i<  m ; i++) {
-			st= new StringTokenizer(br.readLine());
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
 
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			in[b]++;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            in[b]++;
 
-			graph.get(a).add(b);
-		}
-		PriorityQueue<Integer> q= new PriorityQueue<>();
+            graph.get(a).add(b);
+        }
+        PriorityQueue<Integer> q = new PriorityQueue<>();
 
-		int vVisit = 0;
-		for (int i = 1 ; i<=n ; i++) {
-			if (in[i] ==0) {
-				in[i] = -1;
-				q.add(i);
-			}
-		}
+        int vVisit = 0;
+        for (int i = 1; i <= n; i++) {
+            if (in[i] == 0) {
+                in[i] = -1;
+                q.add(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
 
-			while (!q.isEmpty()) {
-				int tempNum = q.peek();
-				q.poll();
-
-
-				System.out.print(tempNum+" ");
-
-				for (int i = 0 ; i< graph.get(tempNum).size() ; i++) {
-
-					int num = graph.get(tempNum).get(i);
-					if (--in[num]==0) {
-						q.add(num);
-					}
-				}
-
-			}
+        while (!q.isEmpty()) {
+            int tempNum = q.peek();
+            q.poll();
 
 
+            sb.append(tempNum).append(" ");
 
-	}
+            for (int i = 0; i < graph.get(tempNum).size(); i++) {
+
+                int num = graph.get(tempNum).get(i);
+                if (--in[num] == 0) {
+                    q.add(num);
+                }
+            }
+
+        }
+
+
+        System.out.println(sb.toString());
+    }
 }
