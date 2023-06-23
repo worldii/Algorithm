7eade95aa239542e9bd778bd95bdc703b9712ad8
bbc2788 <bbc2788@naver.com>
2023-06-24 03:16:28 +0900

    [Bronze I] Title: 배고파(Easy), Time: 164 ms, Memory: 16592 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
@@ -5,39 +5,52 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int n, m;
     public static StringTokenizer st = null;
-    public static int n;
+    public static long[] digit = new long[61];
 
-    public static int getCnt (long num) {
-       String str=  Long.toBinaryString(num);
+    // 상한선 찾기
 
-       for (int i = str.length()-1,  j=0; i>= 0 ; i--, j++) {
-           if (str.charAt(i)=='1') return j;
-        }
-       return 0;
-
-    }
 
     public static void main(String[] args) throws IOException {
 
-        n = Integer.parseInt(br.readLine());
-        for (int  i = 0  ;i< n ; i++) {
-            long num = Long.parseLong(br.readLine());
-            // num 2의 제곱근 판별 & (앤드해서 1이 하나여야 함)
-            long digit = 1; // 2^0
-            while (true) {
-                long num1= digit;
-                long num2= num-num1;
-               // System.out.println(num1+ " " + num2);
-                if (num1 > num2) break;
-                if (Long.bitCount(num2) ==1)
-                {
-                    System.out.println(getCnt(num1) + " "+getCnt(num2));
-                    break;
-                }
-                digit= digit<<1;
+        digit[0] = 1;
+        digit[1] = 2;
+        for (int i = 2; i <= 60; i++) {
+            // digit 짝수 일 때,
+            if (i % 2 == 0) {
+                digit[i] = digit[i / 2] * digit[i / 2];
+            } else {
+                digit[i] = digit[i / 2] * digit[i / 2] * 2;
             }
         }
 
+        n = Integer.parseInt(br.readLine());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            long num = Long.parseLong(br.readLine());
+            long[] arr = findAnswer(num);
+            if (arr[1] == 0) {
+                stringBuilder.append(arr[0]-1).append(" ").append(arr[0]-1).append("\n");
+            } else {
+                long[] arr2 = findAnswer(arr[1]);
+                stringBuilder.append(arr2[0]).append(" ").append(arr[0]).append("\n");
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+
+    private static long[] findAnswer(long num) {
+        int start = 0;
+        int end = 60;
+        while (start <= end ) {
+            int mid = (start + end)/2;
+            if (digit[mid] > num) {
+                end = mid- 1;
+            }
+            else start = mid+1;
+        }
+        return new long [] {end, num - digit[end]};
     }
 }
