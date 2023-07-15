f6209a852d84ac684d880447c4cb8aa05074d324
worldii <bbc2788@naver.com>
2023-07-15 22:19:03 +0900

    [Gold V] Title: 이진 검색 트리, Time: 848 ms, Memory: 30176 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static StringTokenizer st = null;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int num;
+
+        TreeNode(TreeNode left, TreeNode right, int num) {
+            this.left = left;
+            this.right = right;
+            this.num = num;
+        }
+    }
+
+
+    public static TreeNode add(TreeNode cur, int num) {
+        if (cur == null) {
+            cur = new TreeNode(null, null, num);
+            return cur;
+        }
+        if (cur.num > num) {
+
+            cur.left = add(cur.left, num);
+            return cur;
+        } else {
+            cur.right = add(cur.right, num);
+            return cur;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        TreeNode root = null;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            try {
+                int num = Integer.parseInt(br.readLine());
+                list.add(num);
+            } catch (Exception e) {
+                break;
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            root = add(root, list.get(i));
+        }
+
+        postOrder(root);
+    }
+
+    private static void postOrder(TreeNode i) {
+        if (i == null) return;
+        postOrder(i.left);
+        postOrder(i.right);
+        System.out.println(i.num);
+    }
+
+
+}
