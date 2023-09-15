6770a6c01d3a3d84e584a06330e71510f0a24a38
worldii <bbc2788@naver.com>
2023-09-15 17:52:24 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 20284 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -13,27 +13,23 @@ public class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static StringTokenizer st = null;
     public static int n, m;
-    public static int[][] arr = new int[1001][1001];
-    public static boolean checkIsPossible(int start, int end) {
-        boolean[]visited = new boolean[n];
-        Queue<Integer> q= new LinkedList<>();
-
-        q.add(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()){
-            Integer temp = q.peek();
-            q.poll();
-            if (temp == end) return true;
-            for (int i = 0 ; i< n ; i++) {
-                if (arr[temp][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    q.add(i);
-                }
-            }
-        }
-        return false;
+    public static int []parent;
+    public static int findParent(int a, int[]parent) {
+        if (parent[a] == a) return a;
+        parent[a] = findParent(parent[a], parent);
+         return parent[a];
     }
+    public static boolean union (int a, int b) {
+        a= findParent(a, parent);
+        b= findParent(b, parent);
+        if (a!=b) {
+            parent[a] = b;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static int[][] arr = new int[1001][1001];
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
@@ -45,19 +41,28 @@ public class Main {
         }
         st = new StringTokenizer(br.readLine());
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> b = new ArrayList<>();
         for (int i = 0 ; i< m ; i++) {
-            arr.add(Integer.parseInt(st.nextToken()));
+            b.add(Integer.parseInt(st.nextToken())-1);
+        }
+        parent = new int[n];
+
+        for (int i = 0 ; i< n ; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0 ; i< n ; i++) {
+            for (int j = 0 ; j< n ; j++) {
+                if (arr[i][j] == 1) union(i, j);
+            }
         }
         boolean isPossible = true;
-        for (int i = 0 ; i< m-1 ; i++) {
-            if (!checkIsPossible(arr.get(i)-1, arr.get(i+1)-1)) {
+        for (int i = 0 ; i<  m ; i++) {
+            if (findParent(b.get(i), parent) != findParent(b.get(0), parent)) {
                 isPossible = false;
                 break;
             }
         }
-        if (isPossible) System.out.println("YES");
-        else System.out.println("NO");
-
+        System.out.println(isPossible ? "YES" : "NO");
     }
 }
