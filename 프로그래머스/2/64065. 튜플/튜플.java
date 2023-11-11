d59cacdcf75e65a1da1e6b0ccd70749f547aa714
worldii <bbc2788@naver.com>
2023-11-11 19:01:51 +0900

    [level 2] Title: 튜플, Time: 1.07 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,19 +3,21 @@ class Solution {
 
     public Set<Integer> set = new TreeSet<>();
     public int[] solution(String s) {
-        int[] answer = {};
-
-        String temp = s.substring(1,s.length()-1);
-        temp = temp.substring(1, temp.length()-1);
+        
+        String temp = s.substring(2,s.length()-2);
         String[] str= temp.split("\\},\\{");
         List<String[]>  strList = new ArrayList<>();
         int maxNum = 0;
+        
         for (int ii = 0 ; ii< str.length ; ii++) {
             String[] tt = str[ii].split(",");
             strList.add(tt);
             maxNum = Math.max(maxNum, tt.length);
         }
+        
         Collections.sort(strList, (a,b)-> a.length - b.length);
+        
+        int[] answer = {};
         answer = new int[maxNum];
         int idx =0;
         for (int j = 0 ;  j< strList.size() ; j++) {
