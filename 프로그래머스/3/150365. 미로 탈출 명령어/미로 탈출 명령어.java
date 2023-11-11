b8b184602a17ee92119e4bf1c4d0e443e8da8f10
worldii <bbc2788@naver.com>
2023-11-11 18:29:23 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Solution {
-    // l 0 r 1 u 2 d 3 
-    static class Node {
+    class Node {
         int x;
         int y;
         int cnt ;
         String str;
+        
         Node (int x, int y, int cnt, String str ) {
             this.x= x;
             this.y= y;
@@ -19,13 +19,16 @@ class Solution {
     
     public Map<Integer,String> map = new HashMap<>();
     public ArrayList<Node> list = new ArrayList<>();
+    
     public String bfs(int x, int y, int r, int c, int n, int m, int k) {
+        
         PriorityQueue<Node> q= new PriorityQueue<>((a,b)-> {
             if (!a.str.equals(b.str)) {
                 return a.str.compareTo(b.str);
             }
             return a.cnt - b.cnt;
         });
+        
         q.add(new Node(x-1, y-1, 0, ""));
         
         boolean[][][] check = new boolean[n][m][k+1];
@@ -33,18 +36,23 @@ class Solution {
         
         while (!q.isEmpty()){
             Node temp = q.poll();
+            
             if (temp.x == r-1 && temp.y == c-1 && temp.cnt == k )  {
                 return temp.str;
             }
             if (temp.cnt==k) continue;
+            
             for (int i = 0 ; i< 4; i++) {
                 int nextX = dx[i] +temp.x;
                 int nextY = dy[i] + temp.y;
-                if (0>nextX || nextX>=n || 0>nextY || nextY>= m ) continue;
+                
+                if (0 >nextX || nextX>=n || 0 >nextY || nextY>= m ) continue;
+                
                 if (check[nextX][nextY][temp.cnt + 1]) {
                     continue;
                 }
                 check[nextX][nextY][temp.cnt + 1] = true;
+                
                 q.add(new Node(nextX, nextY, temp.cnt+1, temp.str+map.get(i)));
             }
         }
@@ -52,15 +60,34 @@ class Solution {
     }
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
         map.put(0,"d");
         map.put(1,"l");
         map.put(2,"r");
         map.put(3,"u");
         
         int dist = Math.abs(x-r) + Math.abs(y-c);
-        
         if (dist > k || Math.abs(dist - k) %2!=0)  return "impossible";
-        return bfs(x,y,r,c,n,m,k);
+        boolean[][][] check = new boolean[n][m][k+1];
+        dfs(x-1,y-1,0,r,c,n,m,k, "", check);
+        return answer;
+    
+    }
+    private String answer;
+    private void dfs(int curX, int curY, int curK, int r, int c, int n, int m, int k, String dir, boolean[][][]check){
+        if (answer!= null) return;
+        if (check[curX][curY][curK]) return;
+        check[curX][curY][curK] = true;
+        
+        if (curX == r-1 && curY == c-1 && curK == k) {
+            answer = dir;
+            return ;
+        }
+        if (curK>=k) return;
+        for (int i = 0 ; i < 4 ;i++){
+            int nextX = dx[i] + curX;
+            int nextY = dy[i] + curY;
+            if (0 >nextX || nextX>=n || 0 >nextY || nextY>= m ) continue;
+            dfs(nextX, nextY, curK+1, r, c, n, m, k, dir+ map.get(i), check);
+        }
     }
 }
