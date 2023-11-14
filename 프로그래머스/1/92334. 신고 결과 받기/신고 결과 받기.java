355755e75fcdc318fcfb83b5d192c20a6385130b
worldii <bbc2788@naver.com>
2023-11-14 18:06:12 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,22 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public Map<String, HashSet<String>> nameMap = new HashMap<>();
+    public Map<String, ArrayList<String>> nameMap = new HashMap<>();
     public Map<String, Integer> cntMap = new HashMap<>();
+    public Set<String> set = new HashSet<>();
     public int[] solution(String[] id_list, String[] report, int k) {
         for (String id : id_list){
-            nameMap.put(id, new HashSet<>());
+            nameMap.put(id, new ArrayList<>());
             cntMap.put(id, 0);
         }
         
         
         for (int i = 0 ; i< report.length ; i++) {
-            String[] reports = report[i].split(" ");
-            HashSet<String> ids = nameMap.get(reports[0]);
-            if (!ids.contains(reports[1]))  cntMap.put(reports[1],cntMap.get(reports[1])+1);
-
-            ids.add(reports[1]);
-            nameMap.put(reports[0], ids);
+            if (set.add(report[i])) {
+                String[] reports = report[i].split(" ");
+                ArrayList<String> ids = nameMap.get(reports[0]);
+                cntMap.put(reports[1],cntMap.get(reports[1])+1);
+                ids.add(reports[1]);
+                nameMap.put(reports[0], ids);
+            }
         }
         
         int[] answer = new int [id_list.length];
@@ -29,7 +31,7 @@ class Solution {
     
     private int getCount(String name, int k) {
         int count =0;
-        HashSet<String> reporters = nameMap.get(name);
+        ArrayList<String> reporters = nameMap.get(name);
         for (String reporter : reporters) {
             if (cntMap.get(reporter) >= k) count++;
         }
