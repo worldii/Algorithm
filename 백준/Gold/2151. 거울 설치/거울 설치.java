21f1af10c053ba206d879f50f78530b55cd0e861
Jongha Park <bbc2788@naver.com>
2023-12-14 22:41:07 +0900

    [Gold III] Title: 거울 설치, Time: 248 ms, Memory: 19128 KB -BaekjoonHub

---
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static char[][] map;
+
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+    public static List<int[]> door = new ArrayList<>();
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st = null;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        map = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j);
+                if (map[i][j] == '#') {
+                    door.add(new int[]{i, j});
+                }
+            }
+        }
+        System.out.print(
+            Math.min(
+                extracted(n, door.get(0)[0], door.get(0)[1], door.get(1)[0], door.get(1)[1]),
+                extracted(n, door.get(1)[0], door.get(1)[1], door.get(0)[0], door.get(0)[1])
+            ));
+    }
+
+    private static int extracted(int n, int startX, int startY , int endX, int endY) {
+        boolean[][][] visit = new boolean[n][n][4];
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> (a[3] - b[3]));
+
+        for (int i = 0; i < 4; i++) {
+            q.add(new int[]{startX, startY, i, 0});
+            visit[startX][startY][i] = true;
+        }
+
+        while (!q.isEmpty()) {
+            int[] poll = q.poll();
+
+            if (poll[0] == endX && poll[1] == endY) {
+                return poll[3];
+            }
+            int nextX = poll[0] + dx[poll[2]];
+            int nextY = poll[1] + dy[poll[2]];
+            if (nextX < 0 || nextX >= map.length || nextY < 0 || nextY >= map.length) {
+                continue;
+            }
+            if (map[nextX][nextY] == '*') {
+                continue;
+            }
+            if (map[nextX][nextY] == '!' || map[nextX][nextY] == '.' || map[nextX][nextY] == '#') {
+                if (visit[nextX][nextY][poll[2]]) {
+                    continue;
+                }
+                visit[nextX][nextY][poll[2]] = true;
+                q.add(new int[]{nextX, nextY, poll[2], poll[3]});
+            }
+
+            if (map[nextX][nextY] == '!') {
+                for (int i = 0; i < 4; i++) {
+                    // 0 : 1,3 만됨
+                    // 1 : 0,2 만됨
+                    // 2 : 1,3 만됨
+                    // 3 : 0,2 만됨
+                    if ((i + poll[2]) % 2 == 0) {
+                        continue;
+                    }
+                    if (visit[nextX][nextY][i]) {
+                        continue;
+                    }
+                    visit[nextX][nextY][i] = true;
+                    q.add(new int[]{nextX, nextY, i, poll[3] + 1});
+                }
+            }
+
+        }
+        return Integer.MAX_VALUE;
+    }
+
+}
